98b61bfdcc64d9241a8cb1b7c3871cc1cfe7841e
Upinder Singh <upinder.singh@sage.com>
2019-07-23 21:11:29 -0700

    [B-1234] test commint

--- GitTestApp/GitTestApp/Program.cs
+++ GitTestApp/GitTestApp/Program.cs
@@ -16,6 +16,7 @@ namespace GitTestApp
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            int a;
         }
     }
 }
