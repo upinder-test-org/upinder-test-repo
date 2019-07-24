5ff947c70f425d5a6266ccba75ba4d6d0323ef6d
Upinder Singh <upinder.singh@sage.com>
2019-07-23 18:28:56 -0700

    [B-12345] Adding a test repository in github

---
+++ GitTestConsoleProj/GitTestConsoleProj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitTestConsoleProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
